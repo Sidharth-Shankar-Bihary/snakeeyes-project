0d10e2382417b374ccd70915ffb8787b3c4dca60
Sidharth-Shankar-Bihary <sidharth.bihary@gmail.com>
2021-09-07 22:00:32 +0530

    start hello-world server using docker

---
+++ snakeeyes/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object('config.settings')
+    app.config.from_pyfile('settings.py', silent=True)
+
+    @app.route('/')
+    def index():
+        return 'Hello World'
+    
+    return app
